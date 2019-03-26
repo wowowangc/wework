865edd5ef83d2afc178b40132dbebc5a22cbbd10
TracyWangE <seven0907@163.com>
2019-03-26 20:46:01 +0800

    V2.0 简单封装，从文件读json，用例可正确执行

--- src/main/java/com/hogs/wework/contact/Department.java
+++ src/main/java/com/hogs/wework/contact/Department.java
@@ -7,56 +7,50 @@ import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
-public class Department extends Contact{
+public class Department extends Contact {
 
-    //V1
-    public Response list(String id){
-        return given().log().all()
-                .queryParam("access_token", Wework.getToken())
+    //V3 封装Restful
+    public Response list(String id) {
+        reset();
+        Response response = requestSpecification
                 .queryParam("id", id)
-        .when().get("https://qyapi.weixin.qq.com/cgi-bin/department/list")
-        .then().log().all().statusCode(200).extract().response();
+                .when().get("https://qyapi.weixin.qq.com/cgi-bin/department/list")
+                .then().extract().response();
 
+        return response;
     }
 
-
-
-
-
-    //V1
-    public Response create(String departmentName, String parentId){
-        return given().log().all().queryParam("access_token", Wework.getToken())
-                .body("{\n" +
-                        "   \"name\": \""+departmentName+"\",\n" +
-                        "   \"parentid\": "+ parentId+",\n" +
-                        "   \"order\": 1,\n" +
-                        //"   \"id\": 2\n" +
-                        "}")
+    //V2优化为从json读取数据，使用json-path方式读模板，修改变量 -- 弱模板
+    public Response create(String departmentName, String parentId) {
+        reset();
+        String body = JsonPath.parse(this.getClass()
+                .getResourceAsStream("/data/create.json"))
+                .set("$.name", departmentName)
+                .set("$.parentid", parentId)
+                .jsonString();
+        return requestSpecification
+                .body(body)
                 .when().post("https://qyapi.weixin.qq.com/cgi-bin/department/create")
-                .then().log().all().statusCode(200).extract().response();
+                .then().extract().response();
     }
 
-
-
-
-    public Response delete(String id){
+    public Response delete(String id) {
         reset();
         //V3 Restful Contact
         return given().log().all().queryParam("access_token", Wework.getToken())
-        //return requestSpecification
-                .queryParam("id",id)
+                //return requestSpecification
+                .queryParam("id", id)
                 .when().post("https://qyapi.weixin.qq.com/cgi-bin/department/delete")
                 .then().extract().response();
     }
 
-
     //V2优化为从json读取数据，使用json-path方式读模板，修改变量 -- 弱模板
-    public Response update(String departmentName, String parentId){
+    public Response update(String departmentName, String parentId) {
         reset();
         String body = JsonPath.parse(this.getClass()
                 .getResourceAsStream("/data/update.json"))
-                .set("$.name",departmentName)
-                .set("$.parentid",parentId).jsonString();
+                .set("$.name", departmentName)
+                .set("$.parentid", parentId).jsonString();
 
         //V3 Restful Contact
         //return given().log().all().queryParam("access_token", Wework.getToken())
