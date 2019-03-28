d1809fcbb174cc0099ef5049869d3451fb9fe2ba
TracyWangE <seven0907@163.com>
2019-03-28 23:10:28 +0800

    add load conf from yaml

--- src/main/java/com/hogs/wework/WeworkConfig.java
+++ src/main/java/com/hogs/wework/WeworkConfig.java
@@ -1,21 +1,38 @@
 package com.hogs.wework;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.IOException;
+
 public class WeworkConfig {
     //打卡
-    public String agentId="3010011";
-    public String secret="jHSFk3Q-08SsRiqS-v38oCNA0f8493IjLMn_e180Rkg";
-    public String corpid="wwfc0d63ca313ae6d8";
-    public String contactSecret="ktS_fP9wd5uIVwbDYLhl9Dp60T731kQeD-qn0DazZVQ";
+    public String agentId = "3010011";
+    public String secret = "jHSFk3Q-08SsRiqS-v38oCNA0f8493IjLMn_e180Rkg";
+    public String corpid = "wwfc0d63ca313ae6d8";
+    public String contactSecret = "ktS_fP9wd5uIVwbDYLhl9Dp60T731kQeD-qn0DazZVQ";
 
     private static WeworkConfig weworkConfig;
-    public static WeworkConfig getInstance(){
-        if(weworkConfig == null){
-            weworkConfig = new WeworkConfig();
+
+    public static WeworkConfig getInstance() {
+        if (weworkConfig == null) {
+            weworkConfig = load("/conf/WeworkConfig.yml");
+            System.out.println(weworkConfig.agentId);
         }
         return weworkConfig;
     }
 
-    public static void load(String path){
+    public static WeworkConfig load(String path) {
         //TODO read from ymal
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try {
+            //System.out.println(mapper.writeValueAsString(WeworkConfig.getInstance()));
+            return mapper.readValue(WeworkConfig.class.getResourceAsStream(path), WeworkConfig.class);
+         } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
---
+++ src/test/java/com/hogs/wework/WeworkConfigTest.java
@@ -0,0 +1,13 @@
+package com.hogs.wework;
+
+
+import org.junit.jupiter.api.Test;
+
+
+public class WeworkConfigTest {
+
+    @Test
+    public void testLoad() {
+        WeworkConfig.getInstance();
+    }
+}
